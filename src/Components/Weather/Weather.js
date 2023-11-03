937de2799213433baed48135b53ca8a1c34979f4
Neo <ramoroka.neo@gmail.com>
2023-11-03 11:54:13 +0200

    main

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Weather.css';
 import search from "../Assets/search.png";
 import cloudy from "../Assets/cloudy.png";
 import wind from "../Assets/wind.png";
 import humidity from "../Assets/humidity.png";
+import snow from "../Assets/snow.png";
+import storm from "../Assets/storm (2).png";
+import sun from "../Assets/sun (1).png";
+import drizzle from "../Assets/drizzle (1).png";
+import clear from "../Assets/clear-sky.png";
+import mist from "../Assets/mist.png";
+import rain from "../Assets/partly-cloudy.png";
+
 
 
 const Weather = () => {
 
   let api_key = "867e3d508926f295325cf272a95e9001";
+
+  const [wicon,setWicon] = useState(cloudy)
 
   const search = async () => {
     const element = document.getElementsByClassName("cityInput");
@@ -31,6 +41,47 @@ const Weather = () => {
     wind[0].innerHTML = data.wind.speed+" km/h";
     temprature[0].innerHTML = data.main.temp+" degrees";
     location[0].innerHTML = data.name;
+
+    if(data.weather[0].icon==="01d"  || data.weather[0].icon==="01n")
+    {
+      setWicon(clear);
+    }
+   else if(data.weather[0].icon==="02d"  || data.weather[0].icon==="02n")
+    {
+      setWicon(cloudy);
+    }
+    else if(data.weather[0].icon==="03d"  || data.weather[0].icon==="03n")
+    {
+      setWicon(wind);
+    }
+    else if(data.weather[0].icon==="04d"  || data.weather[0].icon==="04n")
+    {
+      setWicon(drizzle);
+    }
+    else if(data.weather[0].icon==="05d"  || data.weather[0].icon==="05n")
+    {
+      setWicon(rain);
+    }
+    else if(data.weather[0].icon==="06d"  || data.weather[0].icon==="06n")
+    {
+      setWicon(storm);
+    }
+    else if(data.weather[0].icon==="07d"  || data.weather[0].icon==="07n")
+    {
+      setWicon(snow);
+    }
+    else if(data.weather[0].icon==="09d"  || data.weather[0].icon==="09n")
+    {
+      setWicon(sun);
+    }
+    else if(data.weather[0].icon==="13d"  || data.weather[0].icon==="13n")
+    {
+      setWicon(cloudy);
+    }
+    else
+    {
+      setWicon(clear);
+    }
 
   }
 
